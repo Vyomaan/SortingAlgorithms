b6a183a07ca36d2f0e66679cd746e9fcc95022fc
Oishik Das <56835743+awsomemanNever@users.noreply.github.com>
2021-10-09 07:42:56 +0530

    Did Some Fixes

--- binary_check.py
+++ binary_check.py
@@ -21,3 +21,15 @@ def binary_search(arr, low, high, x):
     else:
         # Element is not present in the array
         return -1
+    
+# Test array
+arr = [ 2, 3, 4, 10, 40 ]
+x = 10
+ 
+# Function call
+result = binary_search(arr, 0, len(arr)-1, x)
+ 
+if result != -1:
+    print("Element is present at index", str(result))
+else:
+    print("Element is not present in array")
